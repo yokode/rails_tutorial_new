fd587115a1c6780273d95dfdc89229aab28a70f3
yokode <46264353+yokode@users.noreply.github.com>
2026-02-12 14:30:29 +0000

    add bootstrap navbar

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Pin Bootstrap JS (ESM build) for importmap-driven JS
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
