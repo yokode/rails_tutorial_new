4dbaf16fae26138901e0c704a0b41872b723c162
yokode <46264353+yokode@users.noreply.github.com>
2026-02-12 15:28:58 +0000

    add basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/home", to: "pages#home"
   get "/about", to: "pages#about"
