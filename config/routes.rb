aeb6899d650c7b6bba4ded9538d54102129d4705
yokode <46264353+yokode@users.noreply.github.com>
2026-02-17 17:00:05 +0000

    improve devise pages look

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "secure"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/home", to: "pages#home"
   get "/about", to: "pages#about"
