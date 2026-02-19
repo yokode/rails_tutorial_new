8ed2c50380f83077b3e62a03724608b5341acd2b
yokode <46264353+yokode@users.noreply.github.com>
2026-02-19 04:47:30 +0000

    logo helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def logo(size='h2')
+    link_to(root_path, class: "logo #{size}") do
+      "<i class=\"bi bi-safe-fill me-2\"></i> SafePass".html_safe
+    end
+  end
 end
